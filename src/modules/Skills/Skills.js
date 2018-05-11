bff4bbb8ec267a8ce8317bfce57633553f666813
Adam Kulig <adamkulig92@poczta.fm>
2018-05-11 11:24:49 +0200

    new skills

--- src/modules/Skills/Skills.js
+++ src/modules/Skills/Skills.js
@@ -16,18 +16,47 @@ export const Skills = () => {
         <a href='#skills'>
           <h1 className='skills__h1'>Skills</h1>
         </a>
-        <h2 className='skills__h2'>Good:</h2>
-        <div className='skills__logos'>
-          <Logo logo={JS_logo} name='JS_logo'/>
-          <Logo logo={HTML5_logo} name='HTML5_logo'/>
-          <Logo logo={CSS3_logo} name='CSS3_logo'/>
+        <div>
+          <h2 className='skills__h2'>Good:</h2>
+          <div className='skills__logos'>
+            <Logo logo={JS_logo} name='JS_logo'/>
+            <Logo logo={HTML5_logo} name='HTML5_logo'/>
+            <Logo logo={CSS3_logo} name='CSS3_logo'/>
+          </div>
+          <h2 className='skills__h2'>Basic:</h2>
+          <div className='skills__logos'>
+            <Logo logo={React_logo} name='React_logo'/>
+            <Logo logo={Sass_logo} name='Sass_logo'/>
+            <Logo logo={Redux_logo} name='Redux_logo'/>
+          </div>
         </div>
-        <h2 className='skills__h2'>Basic:</h2>
-        <div className='skills__logos'>
-          <Logo logo={React_logo} name='React_logo'/>
-          <Logo logo={Sass_logo} name='Sass_logo'/>
-          <Logo logo={Redux_logo} name='Redux_logo'/>
+        <div className='skills__details'>
+          <div className='skills__details__detail'>
+            <h3 className='skills__details__h3'>ECMAScript 6</h3>
+            <p className='skills__details__p'>When I write javascript code, I use only ES6 syntax.</p>
+          </div>
+          <div className='skills__details__detail'>
+            <h3 className='skills__details__h3'>RWD</h3>
+            <p className='skills__details__p'>I make websites and applications fully responive.</p>
+          </div>
+          <div className='skills__details__detail'>
+            <h3 className='skills__details__h3'>BEM</h3>
+            <p className='skills__details__p'>I use BEM methodology so code is clean and works great with SCSS which I use.</p>
+          </div>
+          <div className='skills__details__detail'>
+            <h3 className='skills__details__h3'>Precise</h3>
+            <p className='skills__details__p'>I am a precision person, I pay special attention to symmetries and details.</p>
+          </div>
+          <div className='skills__details__detail'>
+            <h3 className='skills__details__h3'>Modern positioning</h3>
+            <p className='skills__details__p'>When positioning elements, I always use flexbox. I also learn CSS Grid.</p>
+          </div>
+          <div className='skills__details__detail'>
+            <h3 className='skills__details__h3'>HTML 5</h3>
+            <p className='skills__details__p'>I use only semantic tags in HTML code.</p>
+          </div>
         </div>
+
       </section>
    </ScrollableAnchor>
     
