7cd977c23e5bb32b177e6ac125391f1f2aa9e2b9
Adam Kulig <adamkulig92@poczta.fm>
2018-05-17 18:03:33 +0200

    skills update

---
+++ src/modules/Skills/SingleSkill.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Skills.scss';
+
+export const SingleSkill = (props) => {
+  const { heading, text } = props;
+  return (
+    <div className='skills__details__detail'>
+      <h3 className='skills__details__h3'>{heading}</h3>
+      <p className='skills__details__p'>{text}</p>
+    </div>
+  )
+}
--- src/modules/Skills/Skills.js
+++ src/modules/Skills/Skills.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Skills.scss';
+import { SingleSkill } from './SingleSkill';
 import ScrollableAnchor from 'react-scrollable-anchor';
 import { Logo } from './Logo'
 import HTML5_logo from '../../images/HTML5_logo.png';
@@ -17,13 +18,11 @@ export const Skills = () => {
           <h1 className='skills__h1'>Skills</h1>
         </a>
         <div>
-          <h3 className='skills__h3'>Good</h3>
           <div className='skills__logos'>
             <Logo logo={JS_logo} name='JS_logo'/>
             <Logo logo={HTML5_logo} name='HTML5_logo'/>
             <Logo logo={CSS3_logo} name='CSS3_logo'/>
           </div>
-          <h3 className='skills__h3'>Basic</h3>
           <div className='skills__logos'>
             <Logo logo={React_logo} name='React_logo'/>
             <Logo logo={Sass_logo} name='Sass_logo'/>
@@ -31,32 +30,47 @@ export const Skills = () => {
           </div>
         </div>
         <div className='skills__details'>
-          <div className='skills__details__detail'>
-            <h3 className='skills__details__h3'>ECMAScript 6</h3>
-            <p className='skills__details__p'>When I write javascript code, I use only ES6 syntax.</p>
-          </div>
-          <div className='skills__details__detail'>
-            <h3 className='skills__details__h3'>RWD</h3>
-            <p className='skills__details__p'>I make websites and applications fully responsive.</p>
-          </div>
-          <div className='skills__details__detail'>
-            <h3 className='skills__details__h3'>BEM</h3>
-            <p className='skills__details__p'>I use BEM methodology so code is clean and works great with SCSS which I use.</p>
-          </div>
-          <div className='skills__details__detail'>
-            <h3 className='skills__details__h3'>Precise</h3>
-            <p className='skills__details__p'>I am a precise person, I pay special attention to symmetries and details.</p>
-          </div>
-          <div className='skills__details__detail'>
-            <h3 className='skills__details__h3'>Modern positioning</h3>
-            <p className='skills__details__p'>When positioning elements, I always use flexbox. I also learn CSS Grid.</p>
-          </div>
-          <div className='skills__details__detail'>
-            <h3 className='skills__details__h3'>HTML 5</h3>
-            <p className='skills__details__p'>I use only semantic tags in HTML code.</p>
-          </div>
+          <SingleSkill 
+            heading='REACT.JS'
+            text='My main goal is to learn React so all my projects I do using this framework. I also learn Redux to keep my state out of components.'
+          />
+          <SingleSkill 
+            heading='Clean code'
+            text='I try to write clean code so that it is understandable for every programmer.'
+          />
+          <SingleSkill 
+            heading='Styling'
+            text='I only style using preprocesor SASS exactly the SCSS syntax.'
+          />
+          <SingleSkill 
+            heading='ECMAScript 6'
+            text='When I write javascript code, I use only ES6 syntax.'
+          />
+          <SingleSkill 
+            heading='RWD'
+            text='I make websites and applications fully responsive.'
+          />
+          <SingleSkill 
+            heading='BEM'
+            text='I use BEM methodology so code is clean and works great with SCSS which I use.'
+          />
+          <SingleSkill 
+            heading='Precise'
+            text='I am a precise person, I pay special attention to symmetries and details.'
+          />
+          <SingleSkill 
+            heading='Modern positioning'
+            text='When positioning elements, I always use flexbox. I also learn CSS Grid.'
+          />
+          <SingleSkill 
+            heading='HTML 5'
+            text='I use only semantic tags in HTML code.'
+          />
+          <SingleSkill 
+            heading='Git'
+            text='I know the basics of git and I keep my projects in a remote repository.'
+          />
         </div>
-
       </section>
    </ScrollableAnchor>
     
